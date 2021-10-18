f8b072aced5005fc36f787ddf656dc9074c59fdf
Charles <charlesj68@gmail.com>
2021-10-18 16:14:05 -0600

    Baseline commit
    Pretty much the raw output of Spring Initializr, with a selection of
    useful dependencies chosen.

---
+++ src/main/java/com/example/lifewaydemo/LifewayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.lifewaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifewayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifewayDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lifewaydemo/LifewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifewaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifewayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
